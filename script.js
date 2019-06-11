9d8dee4d804abf391e034fa634ebc4bee51b5ca5
CN0105 <cn0105@CN0105.lan>
2019-06-11 16:05:02 +0100

    my first commit. single player JS is done.

---
+++ script.js
@@ -0,0 +1,25 @@
+let totalScore = 0
+
+document.getElementById("button").addEventListener("click", () => {
+
+const diceGenerate1 = Math.floor(Math.random() * 6) + 1
+const diceImage1 = `img/dice${diceGenerate1}.png`
+totalScore = diceGenerate1+totalScore
+const diceNum1 = `Score: ${totalScore}`
+document.getElementById('p1').setAttribute('src', diceImage1)
+document.getElementById('p1text').innerHTML = diceNum1
+
+if (totalScore >= 21) {
+   alert(`Winner! You reached ${totalScore}`)
+   totalScore = 0
+} else if (diceGenerate1 == 1) {
+   alert(`Game Over! You only reached ${totalScore}`)
+   totalScore = 0
+}
+
+
+
+// const diceGenerate2 = Math.floor(Math.random() * 6) + 1
+// const diceImage2 = `img/dice${diceGenerate2}.png`
+// document.getElementById('p2').setAttribute('src', diceImage2)
+})
